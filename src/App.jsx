c85123c0e5663c906a8ed4b7db2b8a69409c9573
Glauber Rocha <srochaglauber@gmail.com>
2022-06-13 13:46:29 -0300

    Componentes

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
 import { useState } from 'react'
 
-function App() {
+import { Post } from './Post'
+
+export function App() {
 
   return (
-    <h1>Hello World!</h1>
-  )
+    <Post />
+    )
 }
-
-export default App
